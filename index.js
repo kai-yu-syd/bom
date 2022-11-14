f4b18f834bca8b49a060345eabea4f90be05a2a6
Kevin Yu <31651529+kai-yu-syd@users.noreply.github.com>
2022-11-14 15:46:51 +1100

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', async (req, res) => {
     res.status(200).json(data);
   } catch (e) {
     console.error(e)
-    res.status(503).json({ error: 'error' });
+    res.status(503).json(e);
   }
 });
 
